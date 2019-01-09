a8baa110a81ad4acfacae4e9a52b8986cd629da6
Gabe Kangas <gabek@real-ity.com>
2019-01-09 07:22:19 -0800

    Add image optimization to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+echo "Optimizing images..."
+/Applications/ImageOptim.app/Contents/MacOS/ImageOptim ./static/icons/*.png ./themes/HugoMDL/static/images/*.png ./themes/HugoMDL/static/images/*.jpg ./content/blog/images/* >> /dev/null
 
-hugo
+echo "Building site..."
+hugo >> /dev/null
 
 cd public
 cp ../now.json .
-now
+
+echo "Deploying site..."
+now >> /dev/null
+
+echo "Deploying gopher site..."
 mv gopher/post.txt gopher/gophermap
 scp -r gopher/* gabek@sdf.lonestar.org:~/gopher >> /dev/null
+
+echo "Setting domain to the new web instance..."
 now alias
